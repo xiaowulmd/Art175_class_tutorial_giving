be94e21b6d3b232e5536f790becd698f3d335a77
xiaowulmd <xiao.wu@sjsu.edu>
2022-03-09 22:07:22 -0800

    itsalready10iwanttogohome

--- selfportrait.js
+++ selfportrait.js
@@ -1,16 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Page Title</title>
-</head>
-<body>
-
-<h1>This is a Heading</h1>
-<p>This is a paragraph.</p>
-<iframe src="selfportrait.html" style="height:600px;width:600px;" title="Iframe Example"></iframe>
-
-<pre>
-	function setup() {
+function setup() {
   let cnv = createCanvas(600, 600);
 }
 
@@ -74,7 +62,3 @@ function draw() {
   mouth();
   
 }
-</pre>
-
-</body>
-</html>
